daabce1f9b1face9ac38a154a99b9dda5f4c40bd
Tima Kyrgyz <109202799+KatsuTima@users.noreply.github.com>
2022-10-04 13:20:14 +0300

    first save

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <div>
+        <label htmlFor="">Имя</label>
+        <input type="name" />
+        <label htmlFor="">Фамилия</label>
+        <input type="text" />
+        <label htmlFor="">Почта</label>
+        <input type="email" />
+        <button>Вход</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
