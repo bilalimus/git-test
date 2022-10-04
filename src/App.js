76af25ec1fa2b3412a890e699e0fd7b92811800e
Alymbek <alumbek11@gmail.com>
2022-10-04 16:28:11 +0600

    Alymbek

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TestContent from './components/Contents/content';
 
 function App() {
   return (
     <div className="App">
       <h1>test</h1>
+      <TestContent></TestContent>
     </div>
   );
 }
--- src/components/Contents/content.js
+++ src/components/Contents/content.js
@@ -10,3 +10,4 @@ function TestContent() {
         
     )
 }
+export default TestContent
