d72509fe52e5fb671d4ef20828a821f14eb6578f
Alymbek <alumbek11@gmail.com>
2022-10-04 16:22:37 +0600

    alymbek

---
+++ src/components/Contents/content.js
@@ -0,0 +1,12 @@
+import foto from "../../../src/assets/img/1624462887_25-phonoteka_org-p-oboi-na-rabochii-stol-derevo-krasivo-26.jpg"
+
+function TestContent() {
+
+
+    return(<>
+        <h1>Alymbek git</h1>
+        <img src={foto} alt="foto"></img>
+    </>
+        
+    )
+}
