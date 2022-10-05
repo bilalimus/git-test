3d07f48e57ae88963dc33d9f00f2e004afffc9c5
kamchybek <manasbekoovich@gmail.com>
2022-10-05 11:23:15 +0600

    I am add component Kamchybek

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Kamchybek from "./components/Kamchybek/Kamchybek";
 
 function App() {
   return (
     <div className="App">
       <h1>test</h1>
 
-      <Login/>
-
+      <Login />
+      <Kamchybek />
       <TestContent></TestContent>
-      
-
     </div>
   );
 }
---
+++ src/components/Kamchybek/Kamchybek.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Kamchybek() {
+  return (
+    <div>Kamchybek</div>
+  )
+}
+
+export default Kamchybek
